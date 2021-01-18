6f88fbf750e4037e5c4fe84790d9459a4ec4d219
alex <adzulaj@gmail.com>
2021-01-18 17:24:39 +0300

    58

---
+++ Document_circulation/ChangeDocument.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Document_circulation
+{
+    public partial class ChangeDocument : Form
+    {
+        public ChangeDocument()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
